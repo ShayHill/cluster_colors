fix: return correct direction of highest variance

src/cluster_colors/clusters.py
@@ -8,13 +8,10 @@
 
 from __future__ import annotations
 
-from tempfile import tempdir
-
-
 import functools
 from contextlib import suppress
 from operator import itemgetter
-from typing import TYPE_CHECKING, Any, cast
+from typing import TYPE_CHECKING, Any, TypeVar, cast
 
 import numpy as np
 from stacked_quantile import get_stacked_median, get_stacked_medians
@@ -83,6 +80,9 @@ class Member:
         return {Member(v) for v in stacked_vectors if v[-1] > 0}
 
 
+_ClusterT = TypeVar("_ClusterT", bound="Cluster")
+
+
 class Cluster:
     """A cluster of Member instances.
 
@@ -116,6 +116,19 @@ class Cluster:
         self.queue_sub: set[Member] = set()
         # cache calls to self.se
         self._se_cache: dict[int, float] = {}
+        self._vss: FPArray | None = None
+        self._ws: FPArray | None = None
+
+    @classmethod
+    def from_stacked_vectors(
+        cls: type[_ClusterT], stacked_vectors: FPArray
+    ) -> _ClusterT:
+        """Create a Cluster instance from an iterable of colors.
+
+        :param stacked_vectors: An iterable of vectors with a weight axis
+        :return: A Cluster instance
+        """
+        return cls(Member.new_members(stacked_vectors))
 
     @functools.cached_property
     def as_array(self) -> FPArray:
@@ -125,13 +138,39 @@ class Cluster:
         """
         return np.array([member.as_array for member in self.members])
 
+    def _set_vss_ws(self):
+        """Set the cached vss and ws properties."""
+        self._vss, self._ws = np.split(self.as_array, [-1], axis=1)
+
+    @property
+    def vss(self) -> FPArray:
+        """Values for cluster members.
+
+        :return: array of values e.g., (x, y, z) for each member (x, y, z, w)
+        """
+        if self._vss is None:
+            self._set_vss_ws()
+        assert self._vss is not None
+        return self._vss
+
+    @property
+    def ws(self) -> FPArray:
+        """Weights for cluster members.
+
+        :return: array of weights e.g., (w,) for each member (x, y, z, w)
+        """
+        if self._ws is None:
+            self._set_vss_ws()
+        assert self._ws is not None
+        return self._ws
+
     @functools.cached_property
     def vs(self) -> tuple[float, ...]:
         """Values for cluster as a member instance.
 
         :return: tuple of values (x, y, z, w)
         """
-        vss, ws = np.split(self.as_array, [-1], axis=1)
+        vss, ws = self.vss, self.ws
         return tuple(get_stacked_medians(vss, ws))
 
     @functools.cached_property
@@ -140,7 +179,7 @@ class Cluster:
 
         :return: total weight of members
         """
-        _, ws = np.split(self.as_array, [-1], axis=1)
+        ws = self.ws
         return float(np.sum(ws))
 
     @property
@@ -170,7 +209,7 @@ class Cluster:
 
         :return: tuple of eigenvalues and eigenvectors
         """
-        vss, ws = np.split(self.as_array, [-1], axis=1)
+        vss, ws = self.vss, self.ws
         covariance_matrix: FPArray = np.cov(vss.T, aweights=ws.flatten())
         eigenvalues, eigenvectors = np.linalg.eig(covariance_matrix)
         return eigenvalues.astype(float), eigenvectors.astype(float)
@@ -184,18 +223,15 @@ class Cluster:
         return max(self._np_linalg_eig[0])
 
     @functools.cached_property
-    def _axis_of_highest_variance(self) -> FPArray:
+    def _direction_of_highest_variance(self) -> FPArray:
         """Get the first Eigenvector of the covariance matrix.
 
-        :return: Eigenvector of the covariance matrix
+        :return: first Eigenvector of the covariance matrix
 
-        Under a lot of conditions, this will be the axis of highest variance. There are
-        things that will break this, like all colors lying on the (1, 1, 1) line, but
-        you could use ALMOST any axis to split that cluster anyway, so it isn't worth
-        worrying about. There may be other cases where this breaks (silently makes a
-        suboptimal split) but I haven't found them yet.
+        Return the normalized eigenvector with the largest eigenvalue.
         """
-        return self._np_linalg_eig[1][np.argmax(self._np_linalg_eig[0])]
+        eigenvalues, eigenvectors = self._np_linalg_eig
+        return eigenvectors[:, np.argmax(eigenvalues)]
 
     @functools.cached_property
     def quick_error(self) -> float:
@@ -230,7 +266,7 @@ class Cluster:
             a, b = self.members
             return {Cluster([a]), Cluster([b])}
 
-        abc = self._axis_of_highest_variance
+        abc = self._direction_of_highest_variance
 
         def get_rel_dist(rgb: VectorLike) -> float:
             """Get relative distance of rgb from plane Ax + By + Cz + 0.
@@ -300,6 +336,9 @@ def _get_cluster_squared_error(cluster_a: Cluster, cluster_b: Cluster) -> float:
     return _get_squared_error(cluster_a.exemplar, cluster_b.exemplar)
 
 
+_ClustersT = TypeVar("_ClustersT", bound="Clusters")
+
+
 class Clusters:
     """A set of Cluster instances with cached distances and queued updates.
 
@@ -315,6 +354,17 @@ class Clusters:
         self.spans: DistanceMatrix[Cluster]
         self.spans = DistanceMatrix(_get_cluster_squared_error)
         self.add(*clusters)
+
+    @classmethod
+    def from_stacked_vectors(
+        cls: type[_ClustersT], stacked_vectors: FPArray
+    ) -> _ClustersT:
+        """Create a Clusters instance from an iterable of colors.
+
+        :param stacked_vectors: An iterable of vectors with a weight axis
+        :return: A Clusters instance
+        """
+        return cls({Cluster(Member.new_members(stacked_vectors))})
 
     def __iter__(self) -> Iterator[Cluster]:
         """Iterate over clusters.

src/cluster_colors/image_colors.py
@@ -16,16 +16,15 @@ import numpy as np
 from _operator import attrgetter
 from PIL import Image
 
+from cluster_colors.config import CACHE_DIR
 from cluster_colors.cut_colors import cut_colors
 from cluster_colors.kmedians import KMediansClusters
-from cluster_colors.paths import CACHE_DIR, BINARIES_DIR
+from cluster_colors.paths import BINARIES_DIR
 from cluster_colors.pool_colors import pool_colors
 from cluster_colors.stack_vectors import stack_vectors
 
-from src.cluster_colors import kmedians
-
 if TYPE_CHECKING:
-    from cluster_colors.type_hints import NBits, StackedVectors, FPArray
+    from cluster_colors.type_hints import FPArray, NBits, StackedVectors
 
 
 def _stack_image_colors_no_cache(
@@ -119,6 +118,11 @@ def get_image_clusters(
 
 
 def show_clusters(clusters: KMediansClusters, filename_stem: str) -> None:
+    """Create a png with the exemplar of each cluster.
+
+    :param clusters: the clusters to show
+    :param filename_stem: the filename stem to use for the output file
+    """
     width = 1000
     sum_weight = sum(c.w for c in clusters)
     stripes: list[FPArray] = []
@@ -132,6 +136,5 @@ def show_clusters(clusters: KMediansClusters, filename_stem: str) -> None:
     # combine stripes into one array
     image = np.concatenate(stripes, axis=1)
 
-    # image = Image.fromarray(np.hstack(*stripes))  # type: ignore
     image = Image.fromarray(image)
     image.save(BINARIES_DIR / f"{filename_stem}-{len(clusters)}.png")

tests/test_cluster_rgb.py
@@ -6,30 +6,23 @@
 
 from typing import Iterable
 
-import matplotlib
 import numpy as np
-import numpy.typing as npt
 import pytest
-from matplotlib import pyplot as plt
-from PIL import Image
+from matplotlib import pyplot as plt  # type: ignore
 
 # pyright: reportPrivateUsage=false
 import cluster_colors.clusters
-from cluster_colors import kmedians
 from cluster_colors.clusters import Cluster, Member
-from cluster_colors.paths import TEST_DIR
-from cluster_colors.stack_vectors import add_weight_axis, stack_vectors
-from cluster_colors.type_hints import FPArray
+from cluster_colors.stack_vectors import  stack_vectors
 
-matplotlib.use("WebAgg")
 
 
 @pytest.fixture
-def thin_cluster() -> cluster_colors.clusters.Cluster:
+def thin_cluster() -> Cluster:
     """A cluster with all members along the (0,0,0) to (1,1,1) line."""
     colors = np.array([[x, x, x, x] for x in range(10)])
     members = cluster_colors.clusters.Member.new_members(colors)
-    return cluster_colors.rgb_clusters.Cluster(members)
+    return Cluster(members)
 
 
 class TestMemberNewMembers:
@@ -38,7 +31,7 @@ class TestMemberNewMembers:
     def test_one_member_per_color(self) -> None:
         """Return 256 members given 256 colors."""
         colors = stack_vectors(np.random.randint(1, 256, (256, 4), dtype=np.uint8))
-        members = kmedians.Member.new_members(colors)
+        members = Member.new_members(colors)
         assert len(members) == len(colors)
 
 
@@ -60,20 +53,7 @@ class TestCluster:
         cluster = Cluster(members)
         clusters = cluster.split()
         clusters = set.union(*(c.split() for c in clusters))  # type: ignore
-        # show_clusters(clusters)  # type: ignore
-
-
-class TestCallers:
-    """Test the public functions that use Clusters and Members."""
-
-    # def test_get_cluster_tuple(self) -> None:
-    #     """Return exemplar and members for a cluster."""
-    #     colors = np.array([[x, x, x, x] for x in range(1, 10)])
-    #     members = Member.new_members(colors)
-    #     cluster = Cluster(members)
-    #     exemplar, members = cluster_rgb._get_cluster_tuple(cluster)
-    #     assert exemplar == (7, 7, 7)
-    #     assert {tuple(x) for x in members} == {tuple(x) for x in colors}
+        show_clusters(clusters)  # type: ignore
 
 
 def show_clusters(clusters: Iterable[Cluster]) -> None:
@@ -87,22 +67,9 @@ def show_clusters(clusters: Iterable[Cluster]) -> None:
     colors = stack_vectors(colors)  # type: ignore
     for cluster, color in zip(clusters, colors):
         points = cluster.as_array[:, :2]
-        x, y = zip(*points)
-        plt.scatter(x, y, color=color)  # type: ignore
+        xs = [x for x, _ in points]
+        ys = [y for _, y in points]
+        plt.scatter(xs, ys, color=color)  # type: ignore
     plt.show()  # type: ignore
 
 
-# def test_resets_exemplar_age(self) -> None:
-# """Reset exemplar_age to 0 when exemplar is accessed."""
-# cluster = cluster_colors.clusters.Cluster(
-# {cluster_colors.clusters.Member((1, 2, 3), 2), cluster_colors.clusters.Member((4, 5, 6), 1)}
-# )
-# cluster.exemplar_age = 1
-# _ = cluster.exemplar
-# assert cluster.exemplar_age == 0
-
-# # class TestClusterAxis:
-# # def test_1d(self, thin_cluster) -> None:
-# # """Return the axis of the cluster."""
-# # aaa = thin_cluster.axis_of_highest_variance
-# # breakpoint()

tests/test_cut_colors.py
@@ -10,14 +10,28 @@
 # pyright: reportUnknownParameterType=false
 
 import numpy as np
+import numpy.typing as npt
+from typing import Annotated
 
 from cluster_colors import cut_colors, stack_vectors as sv
+
+import pytest
+
+ColorsArray = Annotated[npt.NDArray[np.float_], (-1, 3)]
+
+
+@pytest.fixture(
+    scope="function",
+    params=[np.random.randint(0, 255, (1000, 3), dtype=np.uint8) for _ in range(10)],
+)
+def colors(request) -> ColorsArray:
+    return request.param
 
 
 class TestCutColors:
     # TODO: test cut_colors
-    def test_cut_colors(self):
+    def test_cut_colors(self, colors: ColorsArray):
         """Call cut_colors with 100_000 random colors and pass result to stack_vectors."""
-        colors = np.random.randint(0, 255, (100_000, 3), dtype=np.uint8)
+        # colors = np.random.randint(0, 255, (100_000, 3), dtype=np.uint8)
         colors = sv.stack_vectors(colors)
         aaa = cut_colors.cut_colors(colors, 512)