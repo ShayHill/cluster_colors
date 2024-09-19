9392caed7cd350d9eeca4ff51c68ddf72ebcf1bc
Shay Hill <shay_public@hotmail.com>
2024-09-19 10:03:54 -0500

    feat: raise exceptions into project namespace

--- src/cluster_colors/__init__.py
+++ src/cluster_colors/__init__.py
@@ -1,9 +1,16 @@
 """Raise function names into the project namespace."""
 
+from cluster_colors.cluster_cluster import Cluster
+from cluster_colors.cluster_members import Members
 from cluster_colors.cluster_supercluster import (
     AgglomerativeSupercluster,
     DivisiveSupercluster,
     SuperclusterBase,
+)
+from cluster_colors.exceptions import (
+    EmptySuperclusterError,
+    FailedToMergeError,
+    FailedToSplitError,
 )
 from cluster_colors.image_colors import get_image_clusters, show_clusters
 from cluster_colors.vector_stacker import stack_vectors
@@ -15,4 +22,9 @@ __all__ = [
     "get_image_clusters",
     "show_clusters",
     "stack_vectors",
+    "Members",
+    "Cluster",
+    "EmptySuperclusterError",
+    "FailedToSplitError",
+    "FailedToMergeError",
 ]
